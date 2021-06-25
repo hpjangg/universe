6a5e0b8a1c5bb0d1d83781d1ee34fb73899d20f0
hpjangg <wkdghkvud3302@gmail.com>
2021-06-25 18:49:24 +0900

    BigBang

--- src/com/hpj/universe/Main.java
+++ src/com/hpj/universe/Main.java
@@ -3,6 +3,10 @@ package com.hpj.universe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        bigBang();
+    }
+
+    private static void bigBang() {
+        new Universe();
     }
 }
---
+++ src/com/hpj/universe/Universe.java
@@ -0,0 +1,8 @@
+package com.hpj.universe;
+
+public class Universe {
+    private Long age;
+
+    protected Universe() {
+    }
+}
